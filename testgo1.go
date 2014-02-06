081083c3ed12f99a2f6a4b5bbbf53a5cea62c985
Denevell <aaron@denevell.org>
2014-02-06 13:51:56 +0000

    Update testgo1.go

--- testgo1.go
+++ testgo1.go
@@ -1,7 +1,11 @@
-package main
-
+import "net/http"
 import "fmt"
+import "html"
+import "log"
 
 func main() {
-  fmt.Println("test go 1")
+        http.HandleFunc("/hiya", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Fprintf(w, "Hiiiya, %q", html.EscapeString(r.URL.Path))
+        })
+        log.Fatal(http.ListenAndServe(":8081", nil))
 }
