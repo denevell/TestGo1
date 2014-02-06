f2c2b1ddf6f2eaa7725c369bbd6c1b0f2446b53c
Denevell <aaron@denevell.org>
2014-02-06 15:37:04 +0000

    Update testgo1.go

--- testgo1.go
+++ testgo1.go
@@ -7,7 +7,7 @@ import "log"
 
 func main() {
         http.HandleFunc("/hiya", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "Hiiiya again, %q", html.EscapeString(r.URL.Path))
+                fmt.Fprintf(w, "AND A THIRD EDIT, WOO WEE, %q", html.EscapeString(r.URL.Path))
         })
         log.Fatal(http.ListenAndServe(":8081", nil))
 }
