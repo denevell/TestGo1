3a57b40f4a5d9e81356652c5faac02b90ef138e8
Denevell <aaron@denevell.org>
2014-02-07 15:46:31 +0000

    Update testgo1.go

--- testgo1.go
+++ testgo1.go
@@ -6,7 +6,7 @@ import "log"
 
 func main() {
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "W-w-w-website")
+                fmt.Fprintf(w, "W-w-w-website ~~~~")
         })
         log.Fatal(http.ListenAndServe(":7001", nil))
 }
