4ff7d1913f45d36b036c3e4fa5be95897417aaee
denevell <aaron@denevell.org>
2014-02-15 01:01:28 +0000

    Playing around with templates.

--- testgo1.go
+++ testgo1.go
@@ -3,10 +3,33 @@ package main
 import "net/http"
 import "fmt"
 import "log"
+import "text/template"
+
+type Feeling struct {
+	HowMuch string
+	What    string
+}
+
+var feeling = []Feeling{
+	Feeling{"moderately", "tired"},
+	Feeling{"somewhat", "exuberant"},
+}
+
+var templateStr = `I am feeling {{range $i, $e := .feeling}}{{$e.HowMuch}} {{$e.What}}{{if $i}}.{{else}}, {{end}}{{end}}`
 
 func main() {
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "W-w-w-website ~~~~")
-        })
-        log.Fatal(http.ListenAndServe(":7001", nil))
+	tmpl, err := template.New("test").Parse(templateStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := tmpl.Execute(w,
+			map[string]interface{}{
+				"feeling": feeling,
+			})
+		if err != nil {
+			fmt.Fprintf(w, "Error: "+err.Error())
+		}
+	})
+	log.Fatal(http.ListenAndServe(":7001", nil))
 }
