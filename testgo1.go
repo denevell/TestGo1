92d09f851b4037f128db6373a8b0dae9e4e93c60
denevell <aaron@denevell.org>
2014-03-14 03:52:42 +0000

    Portfolio file now stored in memory.

--- testgo1.go
+++ testgo1.go
@@ -15,6 +15,8 @@ type Project struct {
 	Images []string
 } 
 
+var PORTFOLIO_BYTES []byte = nil
+
 func main() {
         tmpl, err := template.ParseFiles("site.tmpl")
         if err != nil {
@@ -23,14 +25,16 @@ func main() {
 
 	http.Handle("/res/", http.FileServer(http.Dir("")))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		resp, err := http.Get("https://raw.github.com/denevell/BlogPosts/master/portfolio.json")
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-
+		body, err := getPortfolioFile()
+		if err!=nil {
+			fmt.Fprintf(w, "Problem getting portfolio file.")
+			return
+		}
 		projects := make([]Project, 1)
 		err = json.Unmarshal(body, &projects)
 		if err!=nil {
 			fmt.Fprintf(w, "Problem unmarshaling.")
+			return
 		}
                 err = tmpl.Execute(w,
                         map[string]interface{}{
@@ -38,7 +42,24 @@ func main() {
                         })
 		if err != nil {
 			fmt.Fprintf(w, "Error: "+err.Error())
+			return
 		}
 	})
 	log.Fatal(http.ListenAndServe(":7001", nil))
+}
+
+func getPortfolioFile() ([]byte, error) {
+	if PORTFOLIO_BYTES==nil {
+		fmt.Println("Portfolio file doesn't exist")
+		resp, err := http.Get("https://raw.github.com/denevell/BlogPosts/master/portfolio.json")
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		bytes, err := ioutil.ReadAll(resp.Body)
+		PORTFOLIO_BYTES = bytes
+		return bytes, err
+	} else {
+		return PORTFOLIO_BYTES, nil
+	}
 }
